a6776922d1ca44389e324e78562fac7a8f748aa5
Maxwell Banks <maxwell.banks@colorado.edu>
2022-02-05 00:40:59 -0700

    Added comments

--- argparser.go
+++ argparser.go
@@ -2,20 +2,30 @@ package godiscordutil
 
 import (
 	"strings"
+
+	"github.com/bwmarrin/discordgo"
 )
 
+//Verifies that message is meant for this bot
 func IsCommand(message string, flag string) bool {
 	return strings.HasPrefix(message, flag)
 }
 
+//Verifies that message is not from this bot
+func IsOwnMessage(
+	s *discordgo.Session,
+	m *discordgo.MessageCreate,
+) bool {
+	return m.Author.ID == s.State.User.ID
+}
+
+//Takes command, arguments for command, command map
+//Passes command and arguments through given map
+//That maps command to its requisite function
 func CommandToFunc(
 	command string,
 	arguments []string,
-	flag string,
 	cmdMap map[string]func([]string) string,
 ) string {
-	if IsCommand(command, flag) {
-		return cmdMap[command](arguments)
-	}
-	return ""
+	return cmdMap[command](arguments)
 }
