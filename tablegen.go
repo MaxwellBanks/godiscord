492f2683f61f80491f30d95f4aea55cf696f63b4
Maxwell Banks <maxwell.banks@colorado.edu>
2022-02-05 12:18:31 -0700

    Fixing table bugs

--- tablegen.go
+++ tablegen.go
@@ -11,9 +11,9 @@ type RawTable [][]string
 
 // Gets maximum width of each column in table for spacing purposes
 func getColWidths(table RawTable) (colWidths []string) {
-	for i := range table[0] {
+	for i := range table {
 		colWidth := 0
-		for j := range table {
+		for j := range table[0] {
 			if len(table[i][j]) > colWidth {
 				colWidth = len(table[i][j])
 			}
