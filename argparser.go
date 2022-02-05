27f253a1edd9078c943f5254f5c98008f7c5240f
Maxwell Banks <maxwell.banks@colorado.edu>
2022-02-05 02:09:28 -0700

    Added message parse function

--- argparser.go
+++ argparser.go
@@ -8,12 +8,12 @@ import (
 
 type BotFunc func([]string) string
 
-//Verifies that message is meant for this bot
+// Verifies that message is meant for this bot
 func IsCommand(message string, flag string) bool {
 	return strings.HasPrefix(message, flag)
 }
 
-//Verifies that message is not from this bot
+// Verifies that message is not from this bot
 func IsOwnMessage(
 	s *discordgo.Session,
 	m *discordgo.MessageCreate,
@@ -21,9 +21,20 @@ func IsOwnMessage(
 	return m.Author.ID == s.State.User.ID
 }
 
-//Takes command, arguments for command, command map
-//Passes command and arguments through given map
-//That maps command to its requisite function
+// Splits message into command (scrubbed of bot flag)
+// And list of arguments
+func ParseMessage(
+	message string,
+	flag string,
+) (command string, args []string) {
+	msg := strings.Split(message, " ")
+	command, args = strings.TrimPrefix(msg[0], flag), msg[1:]
+	return
+}
+
+// Takes command, arguments for command, command map
+// Passes command and arguments through given map
+// That maps command to its requisite function
 func CommandToFunc(
 	command string,
 	arguments []string,
